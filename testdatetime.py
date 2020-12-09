ad540fbe342e2c955092397af7e8e95e74194324
sirget <37872290+sirget@users.noreply.github.com>
2020-12-09 16:25:45 +0700

    upload

--- testdatetime.py
+++ testdatetime.py
@@ -1,9 +1,14 @@
 import datetime
 
-timestr = '15/05/2020 - 18:04:30'
-time = datetime.datetime.strptime(timestr, "%d/%m/%Y - %H:%M:%S")
+timestr = '15-05-2020 - 18:04:30'
+time = datetime.datetime.strptime(timestr, "%d-%m-%Y - %H:%M:%S")
 print(time)
 x = datetime.datetime.now()
 print(x)
 date = datetime.datetime.timestamp(x)
 print(date)
+sttotime = datetime.datetime.fromtimestamp(date)
+print(sttotime)
+print(type(sttotime))
+strt = str(sttotime)
+print(type(strt))
